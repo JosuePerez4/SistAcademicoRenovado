72a9b0a323370272f77048aaf2277c1ba0c8edb4
JosuePerez4 <josuedanielpg@gmail.com>
2025-04-05 12:22:54 -0500

    Agregando relaciones a el estudiante y demás, y eliminando .vscode del repo

--- sistemaacademico/src/main/java/sistema/academico/entities/Usuario.java
+++ sistemaacademico/src/main/java/sistema/academico/entities/Usuario.java
@@ -5,22 +5,35 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="usuario")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String cedula;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false, unique = true)
     private String apellido;
     private String direccion;
+
+    @Column(nullable = false, unique = true)
     private String correo;
+
     private String telefono;
+    @Column(nullable = false, unique = true)
     private String genero;
     private Date fechaNacimiento;
     private String codigo;
+    @Column(nullable = false, unique = true)
     private String contrasena;
     private boolean estado;
     private String rol;
