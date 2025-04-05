a1a51dff46707269df8e5eac6042af49c9c90936
andresnico44 <ramiroandresnc@ufps.edu.co>
2025-04-04 22:41:55 -0500

    Correccion del Requerimiento de Registro de Calificaciones

--- sistemaacademico/src/main/java/sistema/academico/entities/Curso.java
+++ sistemaacademico/src/main/java/sistema/academico/entities/Curso.java
@@ -2,8 +2,7 @@ package sistema.academico.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -16,10 +15,28 @@ public class Curso {
     private String nombre;
     private String descripcion;
     private String codigo;
-    private Materia materia;
+
+    @ManyToOne
+    private Materia materia; // Relación con Materia
     private int cupoMaximo;
+
     private int semestre;
     private Clase clase;
     private Calificacion calificacion;
     private Estudiante estudiante;
+
+    @OneToMany(mappedBy = "curso", cascade = CascadeType.ALL)
+    private List<Clase> clases; // Relación con Clase (múltiples clases por curso)
+
+    @ManyToMany
+    @JoinTable(
+        name = "curso_estudiante",
+        joinColumns = @JoinColumn(name = "curso_id"),
+        inverseJoinColumns = @JoinColumn(name = "estudiante_id")
+    )
+    private List<Estudiante> estudiantes; // Relación con múltiples estudiantes
+
+    @OneToMany(mappedBy = "curso", cascade = CascadeType.ALL)
+    private List<Calificacion> calificaciones; // Relación con Calificaciones
+    
 }
