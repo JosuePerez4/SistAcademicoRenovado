7994d93cb0670ee748a79193b37991d623105760
andresnico44 <ramiroandresnc@ufps.edu.co>
2025-04-11 19:17:31 -0500

    Cálculo de Promedios y Estadísticas

---
+++ sistemaacademico/src/main/java/sistema/academico/controller/EstadisticasAcademicasController.java
@@ -0,0 +1,42 @@
+package sistema.academico.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sistema.academico.services.EstadisticasAcademicasService;
+
+@RestController
+@RequestMapping("/api/estadisticas-academicas")
+public class EstadisticasAcademicasController {
+
+    @Autowired
+    private EstadisticasAcademicasService estadisticasAcademicasService;
+
+    @GetMapping("/estudiante/{estudianteId}/promedio-general")
+    public ResponseEntity<Double> calcularPromedioGeneralEstudiante(@PathVariable Long estudianteId) {
+        double promedio = estadisticasAcademicasService.calcularPromedioGeneralEstudiante(estudianteId);
+        return ResponseEntity.ok(promedio);
+    }
+
+    @GetMapping("/estudiante/{estudianteId}/cursos-aprobados")
+    public ResponseEntity<Long> contarCursosAprobadosEstudiante(@PathVariable Long estudianteId) {
+        long aprobados = estadisticasAcademicasService.contarCursosAprobadosEstudiante(estudianteId);
+        return ResponseEntity.ok(aprobados);
+    }
+
+    @GetMapping("/estudiante/{estudianteId}/cursos-reprobados")
+    public ResponseEntity<Long> contarCursosReprobadosEstudiante(@PathVariable Long estudianteId) {
+        long reprobados = estadisticasAcademicasService.contarCursosReprobadosEstudiante(estudianteId);
+        return ResponseEntity.ok(reprobados);
+    }
+
+    @GetMapping("/estudiante/{estudianteId}/total-cursos-inscritos")
+    public ResponseEntity<Long> contarTotalCursosInscritosEstudiante(@PathVariable Long estudianteId) {
+        long totalInscritos = estadisticasAcademicasService.contarTotalCursosInscritosEstudiante(estudianteId);
+        return ResponseEntity.ok(totalInscritos);
+    }
+}
