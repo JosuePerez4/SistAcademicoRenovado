319f533fb7fa05b921925d5f9ca22824220940ce
JosuePerez4 <josuedanielpg@gmail.com>
2025-05-28 20:07:01 -0500

    q asco

---
+++ sistemaacademico/src/main/java/sistema/academico/DTO/LoginResponse.java
@@ -0,0 +1,14 @@
+package sistema.academico.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String nombre;
+    private String correo;
+    private String rol;
+}
